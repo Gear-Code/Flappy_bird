2d70dae2e3dff29e081139fdec3f16bac6b86077
Gearcode <gearcoding@gmail.com>
2017-07-03 18:14:59 +0200

    Initial commit

---
+++ Source/Points.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML\Audio.hpp>
+#include <iostream>
+#include "Pipes.h"
+class Points
+{
+public:
+	Points();
+	~Points();
+
+
+	void PointSystem(sf::RenderWindow& window, int FlappyPoints, int PointPositionX, int PointPositionY);
+
+
+	sf::Texture ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE;
+	sf::Sprite sZERO, sONE, sTWO, sTHREE, sFOUR, sFIVE, sSIX, sSEVEN, sEIGHT, sNINE, sONEDouble, sTWODouble, sTHREEDouble;
+};
+
